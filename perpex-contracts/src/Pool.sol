3877d8d78d4422b787ec297850deade58e77640b
José Garção <garcao.random@gmail.com>
2026-03-02 16:04:32 +0000

    contracts: add functions to reserve, release, and get available assets

--- perpex-contracts/src/Pool.sol
+++ perpex-contracts/src/Pool.sol
@@ -6,20 +6,75 @@ import {IPerpex} from "./interfaces/IPerpex.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract Pool is IPool, ERC4626 {
+    using SafeCast for uint256;
+    using SafeCast for int256;
+
+    uint256 private constant WAD = 1e18;
+
     address private _perpex;
+    uint256 private _reservedAssets;
+    uint256 private _maxUtilization;
 
     constructor(address asset_) ERC4626(IERC20(asset_)) ERC20("Perpex Pool Token", "PPT") {}
 
+    function reserveAssets(uint256 assets) external {
+        require(msg.sender == _perpex, "only perpex can reserve assets");
+
+        _reservedAssets += assets;
+
+        uint256 balance = ERC20(asset()).balanceOf(address(this)) * 1e12;
+        int256 pnl = IPerpex(_perpex).pnl();
+
+        if (pnl > balance.toInt256()) {
+            balance = 0;
+        } else if (pnl > 0) {
+            balance -= pnl.toUint256();
+        }
+
+        require(
+            _reservedAssets <= Math.mulDiv(balance, _maxUtilization, WAD * 1e12, Math.Rounding.Floor),
+            "max utilization exceeded"
+        );
+
+        emit AssetsReserved(assets);
+    }
+
+    function releaseAssets(uint256 assets) external {
+        require(msg.sender == _perpex, "only perpex can release assets");
+        require(assets <= _reservedAssets, "cannot release more than reserved");
+
+        _reservedAssets -= assets;
+
+        emit AssetsReleased(assets);
+    }
+
     function totalAssets() public view override returns (uint256) {
-        int256 balance = SafeCast.toInt256(super.totalAssets());
+        int256 balance = ERC20(asset()).balanceOf(address(this)).toInt256() * 1e12;
         int256 pnl = IPerpex(_perpex).pnl();
 
         if (pnl > balance) {
             return 0;
         }
 
-        return SafeCast.toUint256(balance - pnl);
+        return (balance - pnl).toUint256() / 1e12;
+    }
+
+    function availableAssets() public view returns (uint256) {
+        uint256 balance = ERC20(asset()).balanceOf(address(this));
+        if (_reservedAssets == 0) {
+            return balance;
+        }
+
+        uint256 freeAssets = Math.min(balance - _reservedAssets, totalAssets());
+        uint256 minRequiredAssets = Math.mulDiv(_reservedAssets, WAD, _maxUtilization, Math.Rounding.Ceil);
+
+        if (freeAssets <= minRequiredAssets) {
+            return 0;
+        }
+
+        return freeAssets - minRequiredAssets;
     }
 }
--- perpex-contracts/src/interfaces/IPool.sol
+++ perpex-contracts/src/interfaces/IPool.sol
@@ -4,4 +4,13 @@ pragma solidity 0.8.34;
 // TODO: add liquidity
 // TODO: remove liquidity
 
-interface IPool {}
+interface IPool {
+    event AssetsReserved(uint256 assets);
+    event AssetsReleased(uint256 assets);
+
+    function reserveAssets(uint256 assets) external;
+
+    function releaseAssets(uint256 assets) external;
+
+    function availableAssets() external view returns (uint256);
+}
