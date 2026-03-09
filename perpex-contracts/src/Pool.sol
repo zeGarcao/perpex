74324a5f6f04f46269d386594236ef5e950e4757
José Garção <garcao.random@gmail.com>
2026-03-09 18:43:02 +0000

    contracts: (pool) add admin functions and natspec docs

--- perpex-contracts/src/Pool.sol
+++ perpex-contracts/src/Pool.sol
@@ -8,19 +8,88 @@ import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {FixedPointMathLib} from "@solady/utils/FixedPointMathLib.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Pool is IPool, ERC4626 {
+/// @title Pool
+/// @notice ERC4626 vault that backs Perpex positions with USDC liquidity
+/// @dev Tracks reserved assets and enforces a utilization cap for protocol safety
+contract Pool is IPool, ERC4626, AccessControl {
     using SafeCast for uint256;
     using SafeCast for int256;
 
+    //////////////////////////////////////////////////////////////
+    //                        CONSTANTS                         //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Hard cap for max utilization (85%, 18 decimals)
+    uint256 private constant MAX_UTILIZATION = 0.85e18;
+
+    /// @notice Role identifier for admin operations keccak256("ADMIN")
+    bytes32 private constant ADMIN_ROLE = 0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42;
+
+    /// @notice Role identifier for Perpex-only operations keccak256("PERPEX")
+    bytes32 private constant PERPEX_ROLE = 0xeaa91350ea4f7485d1528814ef9ab69999281decb3e768abbcc35926ee435cbb;
+
+    //////////////////////////////////////////////////////////////
+    //                     STATE VARIABLES                      //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Address of the Perpex core contract
     address private _perpex;
+
+    /// @notice Total assets currently reserved for open positions (6 decimals)
     uint256 private _reservedAssets;
+
+    /// @notice Max allowed utilization ratio (18 decimals)
     uint256 private _maxUtilization;
 
-    constructor(address asset_) ERC4626(IERC20(asset_)) ERC20("Perpex Pool Token", "PPT") {}
+    //////////////////////////////////////////////////////////////
+    //                       CONSTRUCTOR                        //
+    //////////////////////////////////////////////////////////////
 
-    function reserveAssets(uint256 assets) external {
-        require(msg.sender == _perpex, POOL__ONLY_PERPEX());
+    /**
+     * @notice Initializes the pool vault and role permissions
+     * @param perpex Address of the Perpex contract
+     * @param asset_ Address of the vault underlying asset (USDC)
+     * @param maxUtilization Initial max utilization ratio (18 decimals)
+     */
+    constructor(address perpex, address asset_, uint256 maxUtilization)
+        ERC4626(IERC20(asset_))
+        ERC20("Perpex Pool Token", "PPT")
+    {
+        require(perpex != address(0), POOL__ZERO_ADDRESS());
+        require(maxUtilization <= MAX_UTILIZATION, POOL__INVALID_MAX_UTILIZATION());
+
+        _perpex = perpex;
+        _maxUtilization = maxUtilization;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+        _grantRole(PERPEX_ROLE, perpex);
+    }
+
+    //////////////////////////////////////////////////////////////
+    //                     ADMIN FUNCTIONS                      //
+    //////////////////////////////////////////////////////////////
+
+    /**
+     * @notice Updates the max utilization ratio
+     * @param maxUtilization New max utilization ratio (18 decimals)
+     */
+    function setMaxUtilization(uint256 maxUtilization) external onlyRole(ADMIN_ROLE) {
+        require(maxUtilization <= MAX_UTILIZATION, POOL__INVALID_MAX_UTILIZATION());
+
+        _maxUtilization = maxUtilization;
+
+        emit MaxUtilizationUpdated(maxUtilization);
+    }
+
+    //////////////////////////////////////////////////////////////
+    //                   POSITION MANAGEMENT                    //
+    //////////////////////////////////////////////////////////////
+
+    /// @inheritdoc IPool
+    function reserveAssets(uint256 assets) external onlyRole(PERPEX_ROLE) {
         require(assets != 0, POOL__INVALID_RESERVE_AMOUNT());
 
         _reservedAssets += assets;
@@ -39,8 +108,8 @@ contract Pool is IPool, ERC4626 {
         emit AssetsReserved(assets);
     }
 
-    function releaseAssets(uint256 assets) external {
-        require(msg.sender == _perpex, POOL__ONLY_PERPEX());
+    /// @inheritdoc IPool
+    function releaseAssets(uint256 assets) external onlyRole(PERPEX_ROLE) {
         require(assets != 0 && assets <= _reservedAssets, POOL__INVALID_RELEASE_AMOUNT());
 
         _reservedAssets -= assets;
@@ -48,17 +117,11 @@ contract Pool is IPool, ERC4626 {
         emit AssetsReleased(assets);
     }
 
-    function totalAssets() public view override returns (uint256) {
-        int256 balance = ERC20(asset()).balanceOf(address(this)).toInt256() * 1e12;
-        int256 pnl = IPerpex(_perpex).totalPnL();
+    //////////////////////////////////////////////////////////////
+    //                      VIEW FUNCTIONS                      //
+    //////////////////////////////////////////////////////////////
 
-        if (pnl > balance) {
-            return 0;
-        }
-
-        return (balance - pnl).toUint256() / 1e12;
-    }
-
+    /// @inheritdoc IPool
     function availableAssets() public view returns (uint256) {
         uint256 balance = ERC20(asset()).balanceOf(address(this));
         if (_reservedAssets == 0) {
@@ -75,6 +138,19 @@ contract Pool is IPool, ERC4626 {
         return freeAssets - minRequiredAssets;
     }
 
+    /// @inheritdoc ERC4626
+    function totalAssets() public view override returns (uint256) {
+        int256 balance = ERC20(asset()).balanceOf(address(this)).toInt256() * 1e12;
+        int256 pnl = IPerpex(_perpex).totalPnL();
+
+        if (pnl > balance) {
+            return 0;
+        }
+
+        return (balance - pnl).toUint256() / 1e12;
+    }
+
+    /// @inheritdoc ERC4626
     function maxRedeem(address owner) public view override returns (uint256) {
         uint256 availableAssets_ = availableAssets();
         if (availableAssets_ == 0) {
@@ -87,6 +163,11 @@ contract Pool is IPool, ERC4626 {
         return FixedPointMathLib.min(availableShares, ownerBalance);
     }
 
+    //////////////////////////////////////////////////////////////
+    //                   INTERNAL FUNCTIONS                     //
+    //////////////////////////////////////////////////////////////
+
+    /// @inheritdoc ERC4626
     function _decimalsOffset() internal pure override returns (uint8) {
         return 12;
     }
--- perpex-contracts/src/interfaces/IPool.sol
+++ perpex-contracts/src/interfaces/IPool.sol
@@ -4,18 +4,73 @@ pragma solidity 0.8.34;
 // TODO: add liquidity
 // TODO: remove liquidity
 
+/// @title IPool
+/// @notice Interface for the Perpex liquidity pool
+/// @dev Manages reserve accounting and available assets for the Perpex protocol
 interface IPool {
+    //////////////////////////////////////////////////////////////
+    //                          EVENTS                          //
+    //////////////////////////////////////////////////////////////
+
+    /**
+     * @notice Emitted when assets are reserved for position backing
+     * @param assets The amount of assets reserved (6 decimals - USDC)
+     */
     event AssetsReserved(uint256 assets);
+
+    /**
+     * @notice Emitted when previously reserved assets are released
+     * @param assets The amount of assets released (6 decimals - USDC)
+     */
     event AssetsReleased(uint256 assets);
 
+    /**
+     * @notice Emitted when the maximum utilization ratio is updated
+     * @param maxUtilization The new max utilization ratio (18 decimals)
+     */
+    event MaxUtilizationUpdated(uint256 maxUtilization);
+
+    //////////////////////////////////////////////////////////////
+    //                          ERRORS                          //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Thrown when a non-Perpex caller tries to invoke a Perpex-only function
     error POOL__ONLY_PERPEX();
+
+    /// @notice Thrown when a zero address is provided where it is not allowed
+    error POOL__ZERO_ADDRESS();
+
+    /// @notice Thrown when max utilization is outside valid bounds
+    error POOL__INVALID_MAX_UTILIZATION();
+
+    /// @notice Thrown when reserving assets would exceed max utilization
     error POOL__MAX_UTILIZATION_EXCEEDED();
+
+    /// @notice Thrown when attempting to release an invalid amount of reserved assets
     error POOL__INVALID_RELEASE_AMOUNT();
+
+    /// @notice Thrown when attempting to reserve an invalid amount of assets
     error POOL__INVALID_RESERVE_AMOUNT();
 
+    //////////////////////////////////////////////////////////////
+    //                        FUNCTIONS                         //
+    //////////////////////////////////////////////////////////////
+
+    /**
+     * @notice Reserves pool assets for open positions
+     * @param assets The amount of assets to reserve (6 decimals - USDC)
+     */
     function reserveAssets(uint256 assets) external;
 
+    /**
+     * @notice Releases previously reserved pool assets
+     * @param assets The amount of assets to release (6 decimals - USDC)
+     */
     function releaseAssets(uint256 assets) external;
 
+    /**
+     * @notice Returns currently available (unreserved) pool assets
+     * @return The amount of available assets (6 decimals - USDC)
+     */
     function availableAssets() external view returns (uint256);
 }
